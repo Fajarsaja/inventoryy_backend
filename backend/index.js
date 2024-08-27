f4719e7a6565fed2e7aaa1231e090be3972fba3f
fajar <ikhromfajar2@gmail.com>
2024-08-27 16:21:24 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
-import express from "express";
-import cors from "cors";
-import UserRoute from  "./routes/UserRoute.js";
+import express from 'express';
+import cors from 'cors';
+import UserRoute from './routes/UserRoute.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swager.js';
-
+import swaggerSpec from './swager.js'; 
 
 const app = express();
 
+
 app.use(cors());
-app.use(UserRoute);
+
+
 app.use(express.json());
 
-// Middleware untuk Swagger
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
+app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('server up and running...'));
+app.listen(5000, () => console.log('Server up and running on port 5000...'));
