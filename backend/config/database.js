f9a992f917c7187a79473603716e72884faa4566
fajar <ikhromfajar2@gmail.com>
2024-11-08 13:20:15 +0700

    UPDATE

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,8 @@ const db = new Sequelize(
   process.env.DB_USER,         
   process.env.DB_PASSWORD,     
   {
-    host: process.env.DB_HOST, 
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
     dialect: process.env.DB_DIALECT 
   }
 );
