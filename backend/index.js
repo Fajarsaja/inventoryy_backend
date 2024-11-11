f413408bba66900ef305b979004474b78a7417cb
fajar <ikhromfajar2@gmail.com>
2024-11-11 17:28:01 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const store = new SessionStore({
 
 (async () => {
   try {
-    await db.sync({ force: false });
+    await db.sync({ alter: true });
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
